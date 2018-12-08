36afdc203c752830e2c0c61f4f4099d7a802d9aa
vinoth <kmvinoth@gmail.com>
2018-12-09 00:44:11 +0100

    check for first three pages working sucess

--- hrk_scrape.py
+++ hrk_scrape.py
@@ -14,65 +14,75 @@ from bs4 import BeautifulSoup
 # remaining_pages_url = "https://www.hochschulkompass.de/en/degree-programmes/study-in-germany-search/advanced-degree-programme-search/search/1/studtyp/3/pn/1.html?tx_szhrksearch_pi1%5Bresults_at_a_time%5D=100"
 
 
-url = "https://www.hochschulkompass.de/en/degree-programmes/study-in-germany-search/advanced-degree-programme-search/search/1/studtyp/3.html?tx_szhrksearch_pi1%5Bresults_at_a_time%5D=100"
+page_num_list = list(range(1, 199))
 
-page_data = requests.get(url)
+url_list_second_page_to_last_page = []
 
-page_soup = BeautifulSoup(page_data.text, 'html.parser')
+url_list_first_page = ["https://www.hochschulkompass.de/en/degree-programmes/study-in-germany-search/advanced-degree-programme-search/search/1/studtyp/3.html?tx_szhrksearch_pi1%5Bresults_at_a_time%5D=100"]
 
-links = page_soup.find_all('a', {'class': 'btn-info btn'})
+""" Construct a list of urls for all the 199 pages """
+for num in page_num_list:
+
+    url = "https://www.hochschulkompass.de/en/degree-programmes/study-in-germany-search/advanced-degree-programme-search/search/1/studtyp/3/pn/"+str(num)+".html?tx_szhrksearch_pi1%5Bresults_at_a_time%5D=100"
+
+    url_list_second_page_to_last_page.append(url)
+
+url_list = url_list_first_page + url_list_second_page_to_last_page
+
+# url = "https://www.hochschulkompass.de/en/degree-programmes/study-in-germany-search/advanced-degree-programme-search/search/1/studtyp/3.html?tx_szhrksearch_pi1%5Bresults_at_a_time%5D=100"
 
 only_learn_more_links = []
-
-for link in links:
-    if 'Learn More' in link.text:
-        print(link['href'])
-        only_learn_more_links.append(link['href'])  # Save href only, for example.
 
 main_dict = {}
 
 counter = 0
-for ul_tag in page_soup.find_all('section', {'class': 'result-box'}):
-    counter = counter + 1
-    results = {}
-    print("Course name = ", ul_tag.h2.text)
-    results["Course_Name"] = ul_tag.h2.text
 
-    dum_var = counter-1
-    results["Learn_more_url"] = "https://www.hochschulkompass.de" + only_learn_more_links[dum_var]
+small_url = [url_list[0], url_list[1], url_list[2]]
 
-    for li_tag in ul_tag.find_all('ul', {'class': 'info list-inline'}):
+for url in small_url:
 
-        for span_tag in li_tag.find_all('li'):
-            field = span_tag.find('span', {'class': 'title'}).text
-            value = span_tag.find('span', {'class': 'status'}).text
-            print("field = ", field)
-            print("value = ", value)
-            field = field.replace(" ", "_")
-            results[field] = value
-        main_dict[counter] = results
+    page_data = requests.get(url)
+
+    page_soup = BeautifulSoup(page_data.text, 'html.parser')
+
+    # find all the Learn More links in the page
+    links = page_soup.find_all('a', {'class': 'btn-info btn'})
+
+    for link in links:
+        if 'Learn More' in link.text:
+            print(link['href'])
+            only_learn_more_links.append(link['href'])  # Save href only
+
+    for ul_tag in page_soup.find_all('section', {'class': 'result-box'}):
+        counter = counter + 1
+        results = {}
+        print("Course name = ", ul_tag.h2.text)
+        results["Course_Name"] = ul_tag.h2.text
+
+        # counter -1, because list index starts from 0
+        print("COUNTER = ", counter)
+        dum_var = counter-1
+        print("DUM_VAR = ", dum_var)
+        results["Learn_more_url"] = "https://www.hochschulkompass.de" + only_learn_more_links[dum_var]
+
+        for li_tag in ul_tag.find_all('ul', {'class': 'info list-inline'}):
+
+            for span_tag in li_tag.find_all('li'):
+                field = span_tag.find('span', {'class': 'title'}).text
+                value = span_tag.find('span', {'class': 'status'}).text
+                print("field = ", field)
+                print("value = ", value)
+                field = field.replace(" ", "_")
+                results[field] = value
+            main_dict[counter] = results
 
 cols = list(main_dict.keys())
-
-print(cols)
 
 with open("first_page_res.json", "w") as f:
     json.dump(main_dict, f, ensure_ascii=False, indent=4)
 
 
-# page_num_list = list(range(1, 199))
-#
-# url_list_second_page_to_last_page = []
-#
-# url_list_first_page = ["https://www.hochschulkompass.de/en/degree-programmes/study-in-germany-search/advanced-degree-programme-search/search/1/studtyp/3.html?tx_szhrksearch_pi1%5Bresults_at_a_time%5D=100"]
-#
-# for num in page_num_list:
-#
-#     url = "https://www.hochschulkompass.de/en/degree-programmes/study-in-germany-search/advanced-degree-programme-search/search/1/studtyp/3/pn/"+str(num)+".html?tx_szhrksearch_pi1%5Bresults_at_a_time%5D=100"
-#
-#     url_list_second_page_to_last_page.append(url)
-#
-# url_list = url_list_first_page + url_list_second_page_to_last_page
+
 #
 # main_dict = {}
 #
