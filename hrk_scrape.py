4c5047b8e6de534fac65f74e680bfdfe8d246b0a
vino <vinothkumar.mohanakrishnan@charite.de>
2019-01-03 12:22:30 +0100

    complete script for getting basic info

--- hrk_scrape.py
+++ hrk_scrape.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import json
+import random
 from bs4 import BeautifulSoup
 
 # First 100 results
@@ -39,9 +40,24 @@ counter = 0
 
 small_url = [url_list[0], url_list[1], url_list[2]]
 
-for url in small_url:
+start_time = time.time()
+
+reqs = 0
+
+for url in url_list:
 
     page_data = requests.get(url)
+
+    # time.sleep(random.randint(2, 4))
+
+    reqs = reqs + 1
+
+    elapsed_time = time.time() - start_time
+
+    print('Request:{}; Frequency: {} requests/s'.format(reqs, reqs / elapsed_time))
+
+    if page_data.status_code != 200:
+        print('Request: {}; Status code: {}'.format(reqs, page_data.status_code))
 
     page_soup = BeautifulSoup(page_data.text, 'html.parser')
 
@@ -50,19 +66,19 @@ for url in small_url:
 
     for link in links:
         if 'Learn More' in link.text:
-            print(link['href'])
+            # print(link['href'])
             only_learn_more_links.append(link['href'])  # Save href only
 
     for ul_tag in page_soup.find_all('section', {'class': 'result-box'}):
         counter = counter + 1
         results = {}
-        print("Course name = ", ul_tag.h2.text)
+        # print("Course name = ", ul_tag.h2.text)
         results["Course_Name"] = ul_tag.h2.text
 
         # counter -1, because list index starts from 0
-        print("COUNTER = ", counter)
+        # print("COUNTER = ", counter)
         dum_var = counter-1
-        print("DUM_VAR = ", dum_var)
+        # print("DUM_VAR = ", dum_var)
         results["Learn_more_url"] = "https://www.hochschulkompass.de" + only_learn_more_links[dum_var]
 
         for li_tag in ul_tag.find_all('ul', {'class': 'info list-inline'}):
@@ -70,75 +86,13 @@ for url in small_url:
             for span_tag in li_tag.find_all('li'):
                 field = span_tag.find('span', {'class': 'title'}).text
                 value = span_tag.find('span', {'class': 'status'}).text
-                print("field = ", field)
-                print("value = ", value)
+                # print("field = ", field)
+                # print("value = ", value)
                 field = field.replace(" ", "_")
                 results[field] = value
             main_dict[counter] = results
 
 cols = list(main_dict.keys())
 
-with open("first_page_res.json", "w") as f:
+with open("master_res_all.json", "w") as f:
     json.dump(main_dict, f, ensure_ascii=False, indent=4)
-
-
-
-#
-# main_dict = {}
-#
-# for url in url_list:
-#
-#     page_data = requests.get(url)
-#
-#     page_soup = BeautifulSoup(page_data.text, 'html.parser')
-#
-#     counter = 0
-#
-#     for ul_tag in page_soup.find_all('section', {'class': 'result-box'}):
-#         counter = counter + 1
-#         results = {}
-#         print("Course name = ", ul_tag.h2.text)
-#         results["Course_Name"] = ul_tag.h2.text
-#
-#         for li_tag in ul_tag.find_all('ul', {'class': 'info list-inline'}):
-#
-#             for span_tag in li_tag.find_all('li'):
-#
-#                 field = span_tag.find('span', {'class': 'title'}).text
-#                 value = span_tag.find('span', {'class': 'status'}).text
-#                 print("field = ", field)
-#                 print("value = ", value)
-#                 field = field.replace(" ", "_")
-#                 results[field] = value
-#             main_dict[counter] = results
-#
-# cols = list(main_dict.keys())
-#
-# with open("first_page_res.json", "w") as f:
-#     json.dump(main_dict, f, ensure_ascii=False, indent=4)
-
-
-
-
-# print(url_list)
-
-# print(len(url_list))
-
-# https://www.hochschulkompass.de/en/degree-programmes/study-in-germany-search/advanced-degree-programme-search/search/1/studtyp/3/pn/2.html?tx_szhrksearch_pi1%5Bresults_at_a_time%5D=100
-
-#
-# for result in first_page_results:
-#     if len(result.attrs) == 2:
-#         print("RESULTS")
-#         print("*************************************************")
-#         print(result)
-#         print("COURSE NAME")
-#         print("*************************************************")
-#         print(result.h2)
-#         break
-
-
-
-
-
-
